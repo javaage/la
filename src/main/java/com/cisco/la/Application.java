dae689de89bdad0aaaa2e52cb9b12b9cee132bfb
javaage <subing.xiao@pactera.com>
2018-01-11 14:55:53 +0800

    test

--- src/main/java/com/cisco/la/Application.java
+++ src/main/java/com/cisco/la/Application.java
@@ -17,17 +17,13 @@
 package com.cisco.la;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "com.cisco.la")
-@EnableCaching
-@EnableTransactionManagement
+//@EnableAutoConfiguration
+//@ComponentScan(basePackages = "com.cisco.la")
+//@EnableCaching
+//@EnableTransactionManagement
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
